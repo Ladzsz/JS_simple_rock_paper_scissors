ddfc19f63991d60fcb015550f58bfec8b6f0db8c
London Mason <londonmason602@gmail.com>
2024-05-22 10:45:41 -0400

    finisehd the game but the or operator is acting like an and

--- ROSHAMBOscript.js
+++ ROSHAMBOscript.js
@@ -3,14 +3,6 @@
 
 function GetComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
-    if (choice === 1) {
-        choice = "rock"
-        } else if (choice === 2) {
-            choice = "paper"
-        } else {
-            choice = "scissors"
-        }
-
     return choice
 }
 
@@ -19,14 +11,6 @@ function GetComputerChoice() {
 
 function GetPlayerChoice() {
     let player_choice = parseInt(prompt("choose either Rock(1) paper(2) scissors(3)"));
-    if (player_choice === 1) {
-    player_choice = "rock"
-    } else if (player_choice === 2) {
-        player_choice = "paper"
-    } else {
-        player_choice = "scissors"
-    }
-
     return player_choice
 
 }
@@ -39,38 +23,52 @@ let bot_score = 0
 
 //controlling the logic with a while loop
 
-while (player_score < 3 || bot_score < 3) {
+while (player_score < 5 || bot_score < 5) {
     let player_choice = GetPlayerChoice();
     let bot_choice = GetComputerChoice();
 
     if (player_choice == bot_choice) {
         console.log("DRAW No points for either side.")
         console.log(`Player score: ${player_score}\n Bot score: ${bot_score}`)
-    } else if (player_choice == 1 && bot_choice == 2) {
-        console.log("1 point for COM")
-        console.log(`Player score: ${player_score}\n Bot score: ${bot_score}`)
+    }
+    
+    else if (player_choice == 1 && bot_choice == 2) {
+        console.log("Paper beats Rock. 1 point for COM")
         bot_score ++
-    } else if (player_choice == 1 && bot_choice == 3) {
-        console.log("1 point for Player")
         console.log(`Player score: ${player_score}\n Bot score: ${bot_score}`)
+    } 
+    
+    else if (player_choice == 1 && bot_choice == 3) {
+        console.log("Rock beats Scissors. 1 point for Player")
         player_score ++
-    } else if (player_choice == 2 && bot_choice == 1) {
-        console.log("1 point for Player")
         console.log(`Player score: ${player_score}\n Bot score: ${bot_score}`)
+    } 
+    
+    else if (player_choice == 2 && bot_choice == 1) {
+        console.log("Paper beats Rock. 1 point for Player")
         player_score ++
-    } else if (player_choice == 2 && bot_choice == 3) {
-        console.log("1 point for COM")
         console.log(`Player score: ${player_score}\n Bot score: ${bot_score}`)
+    } 
+    
+    else if (player_choice == 2 && bot_choice == 3) {
+        console.log("Scissors beats Paper. 1 point for COM")
         bot_score ++
-    } else if (player_choice == 3 && bot_choice == 1) {
-        console.log("1 point for COM")
         console.log(`Player score: ${player_score}\n Bot score: ${bot_score}`)
+    } 
+    
+    else if (player_choice == 3 && bot_choice == 1) {
+        console.log("Rock beats Scissors. 1 point for COM")
         bot_score ++
-    } else if (player_choice == 3 && bot_choice == 2) {
-        console.log("1 point for Player")
         console.log(`Player score: ${player_score}\n Bot score: ${bot_score}`)
-        player_score ++  
-    } else {
+    } 
+    
+    else if (player_choice == 3 && bot_choice == 2) {
+        console.log("Paper beats Rock. 1 point for Player")
+        player_score ++
+        console.log(`Player score: ${player_score}\n Bot score: ${bot_score}`)  
+    } 
+    
+    else {
         console.log("ERROR: Enter (1) for rock, (2) for paper, (3) for scissors.")
     }        
 }
